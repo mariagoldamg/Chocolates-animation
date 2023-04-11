56b06b6bef21e4bc086511820de50b267daf4800
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-11 13:41:49 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+gsap.from('.picture', {y:30, duration:1, opacity:0});
+gsap.from ('.links', {opacity:0, duration:2, delay:1, stagger:.6});
+gsap.from ('.btn', {y:30, duration:1, opacity:0, delay:3})
+
+/*gsap.from ('.picture', {y:100, ease:"bounce", duration:3});
+gsap.from ('.links', {opacity:0, duration:2, delay:1});
+gsap.from ('.btn', {y:30, duration:1, opacity:0, delay:2, opacity:0})*/
